25689641f7e50242c6ad5a14f290e1b9daf06a22
KeltarHelviett <keltarhelviett@gmail.com>
2018-05-11 22:16:08 +1000

    Registered ForestSN app. Included ForestSN url config

--- Forest/settings.py
+++ Forest/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'ForestSN.apps.ForestsnConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
