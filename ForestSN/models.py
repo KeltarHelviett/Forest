206a4b9325a0fda06a891da9b81162e9d09ddbc8
KeltarHelviett <keltarhelviett@gmail.com>
2018-05-16 00:12:39 +1000

    Autocreat UserProfile on User creation

--- ForestSN/models.py
+++ ForestSN/models.py
@@ -2,4 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.get_or_create(user=instance)
+
+models.signals.post_save.connect(create_user_profile, sender=User)
