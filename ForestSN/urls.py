fef550c3863be8505adc8135bc573a6c1df334a9
KeltarHelviett <keltarhelviett@gmail.com>
2018-05-16 18:51:21 +1000

    Added url redirections for authenticated users and shortcut url  /me/

--- ForestSN/urls.py
+++ ForestSN/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', views.log_in, name='login'),
     path('', include('django.contrib.auth.urls')),
     path('signup/', views.sign_up, name='sign_up'),
     path('profile/<int:user_id>', views.profile, name='profile_page'),
+    path('me/', views.me, name='me')
 ]
--- ForestSN/views.py
+++ ForestSN/views.py
@@ -2,14 +2,37 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
+from django.contrib.auth.views import login as login_view
 
+class RedirectAuthenticatedUser(object):
+
+    def __init__(self, view):
+        self.view = view
+
+    def __call__(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('/profile/{}'.format(request.user.id))
+        return self.view(request, *args, **kwargs)
+
+@RedirectAuthenticatedUser
+def log_in(request):
+    """Log in page"""
+
+    return login_view(request)
+
+@RedirectAuthenticatedUser
 def index(request):
     """Index page"""
+    if request.user.is_authenticated:
+        return redirect('/profile/{}'.format(request.user.id))
 
     return render(request, 'ForestSN/index.html')
 
+@RedirectAuthenticatedUser
 def sign_up(request):
     """Sign up page"""
+    if request.user.is_authenticated:
+        return redirect('/profile/{}'.format(request.user.id))
 
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -27,9 +50,15 @@ def sign_up(request):
     context = {'form': form}
     return render(request, 'ForestSN/signup.html', context=context)
 
-def  profile(request, user_id):
+def profile(request, user_id):
     """Profile page"""
     owner = get_object_or_404(User, pk=user_id)
     context = {'owner': owner}
 
     return render(request, 'ForestSN/profile.html', context=context)
+
+def me(request): #pylint: disable=C0103
+    """Authenticated user's profile.
+    Shortcut for /profile/request.user.id"""
+
+    return profile(request, request.user.id)
