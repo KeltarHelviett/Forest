227726e17398c31c972056d34a63549c8d361032
KeltarHelviett <keltarhelviett@gmail.com>
2018-05-19 14:00:19 +1000

    Js moved to profile.js

--- ForestSN/static/ForestSN/profile.js
+++ ForestSN/static/ForestSN/profile.js
@@ -13,5 +13,40 @@ $(document).ready(function () {
         $('#save-profile-btn').css('display', 'none');
         $('#edit-btn').css('display', 'inline');
         $('#profile-fieldset').prop('disabled', true);        
-    })
+    });
+    var csrftoken = $("[name=csrfmiddlewaretoken]").val();
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!this.crossDomain) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken);
+            }
+        }
+    });
+    $('#profile-img-form').submit(function (e) {
+        e.preventDefault();
+        let data = new FormData($("#profile-img-form")[0]);
+        console.log($("#profile-img-form"));
+        $.ajax({
+            url: "/media/upload_profile_img",
+            type: "POST",
+            data: data,
+            cache: false,
+            processData: false,
+            dataType: "json",
+            contentType: false,
+            success: function (data) {
+                $("#profile-img").attr('src', data.url);
+            },
+            error: function(data) {
+            },
+            "cstfmiddlewaretoken": $("[name=csrfmiddlewaretoken]").val()
+        });
+        return false;
+    });
+    $('#upload-profile-img-btn').click(function () {
+        $('#profile-img-input').click();
+    });
+    $('#profile-img-input').change(function (d) {
+        $('#profile-img-form').submit();
+    });
 })
