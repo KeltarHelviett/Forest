18c1ed21f9287fd320472acce7a9f2c651299549
KeltarHelviett <keltarhelviett@gmail.com>
2018-05-24 18:48:18 +1000

    Added wall_owner to Post model. Registered Post model in admin site

--- ForestSN/models/post.py
+++ ForestSN/models/post.py
@@ -2,19 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    author_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authors')
+    wall_owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wall_owners')
     pub_date = models.DateTimeField('publication date', auto_now_add=True)
     parent_post = models.ForeignKey(
-        'self',
-        on_delete=models.CASCADE,
-        related_name='parent_post_set',
-        null=True
+        'self', on_delete=models.CASCADE,
+        related_name='parent_post_set', null=True,
+        blank=True, default=None
     )
     root_post = models.ForeignKey(
-        'self',
-        on_delete=models.CASCADE,
-        related_name='root_post_set',
-        null=True
+        'self', on_delete=models.CASCADE,
+        related_name='root_post_set', null=True,
+        blank=True, default=None
     )
     text = models.TextField(max_length=1000, blank=True)
     # TODO attachments
