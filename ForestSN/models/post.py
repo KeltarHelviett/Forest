c0532c80c60dc4c75fe27dfee4474107ded4d8aa
KeltarHelviett <keltarhelviett@gmail.com>
2018-05-25 23:43:35 +1000

    Added methods to Post for a better apperance in admin site

--- ForestSN/models/post.py
+++ ForestSN/models/post.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.template.defaultfilters import truncatechars
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='authors')
@@ -17,3 +18,14 @@ class Post(models.Model):
     )
     text = models.TextField(max_length=1000, blank=True)
     # TODO attachments
+
+    @property
+    def short_text(self):
+        return truncatechars(self.text, 20)
+
+    def __str__(self):
+        return """{}: {} {}""".format(
+            self.author.username,
+            truncatechars(self.text, 40),
+            self.pub_date.strftime('%Y-%m-%d')
+        )
