3eb79b5fb0f24d0f5eb956a7c61112aa5640f75f
KeltarHelviett <keltarhelviett@gmail.com>
2018-05-25 23:41:03 +1000

    Added shortcut profile_img_url

--- ForestSN/models/user_profile.py
+++ ForestSN/models/user_profile.py
@@ -8,6 +8,11 @@ class UserProfile(models.Model):
     last_name = models.CharField(max_length=150, blank=True, null=True)
     profile_img = models.ForeignKey(UserImage, on_delete=models.SET_NULL, null=True)
 
+    def profile_img_url(self):
+        if self.profile_img is not None:
+            return self.profile_img.img.url
+        return 'https://i.pinimg.com/564x/5b/70/d1/5b70d179c4e8a58fa4f2ac6ae0ea5ba1.jpg'
+
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.get_or_create(user=instance)
