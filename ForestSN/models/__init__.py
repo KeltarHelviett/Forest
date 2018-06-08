24427562b8acc52cd72e2518a2a53953164fe44b
KeltarHelviett <keltarhelviett@gmail.com>
2018-06-09 00:46:50 +1000

    Create migrations for oauth and external social network

--- ForestSN/models/__init__.py
+++ ForestSN/models/__init__.py
@@ -1,3 +1,5 @@
 from .user_image import UserImage
 from .user_profile import UserProfile
 from .post import Post
+from .external_sn import ExternalSocialNetwork, ExternalSocialNetworkSession
+from .oauth2 import AccessToken, AuthorizationCode
